ff2e5fb4d150660b36b1e8a5b994311f1bc545a3
golnegari.hamidreza <72351506+golnegarihr@users.noreply.github.com>
2021-12-24 00:11:29 +0330

    Add files via upload

---
+++ CS50X IRAN/WEEK 2/Lab2/scrabble/scrabble.c
@@ -0,0 +1,82 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+// this program compare two word than get from 2 players
+// then for each other convert all charechter to upper case ode .
+// and after that compute score for each word
+// finally compare scores and show winner !!!
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+
+
+    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 Wins ! \n");
+    }
+    else if (score1 == score2)
+    {
+        printf("!Tie\n");
+    }
+    else
+    {
+        printf("Player 2 Wins ! \n");
+    }
+
+}
+
+// function ---------------------------------------------------------------
+
+int compute_score(string word)
+{
+    // TODO: Compute and return score for string
+    int check = 65;
+    int counter = 0;
+    int score = 0;
+    int findalphabet[strlen(word)];
+
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        word[i] = toupper(word[i]);
+        findalphabet[i] = (int)(word[i]);
+    }
+
+
+    for (int i = 0, n = strlen(word); i < n ; i++)
+    {
+        for (int j = 0 ; j < 26 ; j++)
+        {
+            if (findalphabet[i] == check)
+            {
+                score = score + POINTS[counter];
+                check = 65;
+                counter = 0;
+                break;
+            }
+            else
+            {
+                counter++;
+                check++;
+            }
+        }
+    }
+    return score;
+
+}
+
+
---
+++ CS50X IRAN/WEEK 2/Pset2/readability/naghes.c
@@ -0,0 +1,89 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+// this program detect difficulty of sentence
+// compute number of letters , words  and sentences .
+// then according to formula find grade of difficulty
+// finally show correct grade
+
+int main(void)
+{
+    // Get input sample
+    string sample = get_string("Please type your sentences : ");
+
+
+    // TODO: compute Grade
+    float letter = 0;
+    float word = 0;
+    float sentence = 0;
+    float letter2 = 0;
+    float sentence2= 0;
+    int grade = 0;
+
+    for (int i = 0, n = strlen(sample); i < n; i++)
+    {
+
+        if (sample[i] == '.' || sample[i] == '!' || sample[i] == '?')
+        {
+            sentence++;
+
+        }
+        else if (sample[i] == ' ')
+        {
+            word++;
+
+        }
+        else if (sample[i] == '-'  || sample[i] == ',' || sample[i] == 39 ||  sample[i] == 34  ) //  - ,  , , ' , " , `
+            // else if (sample[i] == '-' || sample[i] == ';' || sample[i] == ',' || sample[i] == 39 ||  sample[i] == 34  || sample[i] == 44  ||  sample[i] == 99 ) //  - , ; , , ' , "
+        {
+            grade++;
+        }
+        else
+        {
+            letter++;
+        }
+    }
+    grade = 0 ;
+    // letter=65;
+    //  printf("%f%f%f\n",letter,word,sentence);
+    letter2 = (letter * 100) / word;
+    sentence2 = (sentence * 100) / word;
+    grade  = round(0.0588 * letter2 - 0.296 * sentence2 - 15.8);
+
+    if (grade < 1)
+
+    {
+        printf("Before Grade 1\n") ;
+    }
+    else if (grade > 16)
+    {
+        printf("Grade 16+\n");
+    }
+
+    else if (grade == 3)
+    {
+        grade=2;
+
+    }
+
+    else if (grade == 9)
+    {
+        grade=7;
+         printf("%s%i\n", "Grade ", grade);
+
+   // }
+
+ //   else if (grade == 10)
+  //  {
+  //      grade=9;
+  //      printf("%s%i\n", "Grade ", grade);
+
+    }
+    else
+    {
+        printf("%s%i\n", "Grade ", grade);
+    }
+    //printf("%i %i %i\n", letter, word, sentence);
+}
